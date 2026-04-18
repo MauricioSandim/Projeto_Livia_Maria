47979ebce5987d48528bc161a14aa79d24fd7215
MauricioSandim <mauriciosandim06@gmail.com>
2026-04-18 19:21:02 -0300

    Adição método de soma na classe A

--- ClasseA.java
+++ ClasseA.java
@@ -6,27 +6,33 @@ public class ClasseA {
     public int getA1() {
         return A1;
     }
+
     public void setA1(int a1) {
         A1 = a1;
     }
-    
+
     public float getA2() {
         return A2;
     }
+
     public void setA2(float a2) {
         A2 = a2;
     }
 
-    public void MA1(){
+    public void MA1() {
         System.out.println("MA1");
-    } 
+    }
 
-    public void MA2(){
+    public void MA2() {
         System.out.println("MA2");
     }
 
     public void MA3() {
         System.out.println("Alteração a classe A partir do clone.");
+    }
+
+    public static int soma(int a, int b) {
+        return a + b;
     }
 
 }
